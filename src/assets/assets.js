9b73689799c3faffa506daa8ed9d7b8080dbe0a7
yasirrehman274 <yasirrehman274@gmail.com>
2025-02-28 23:27:40 +0500

    AboutSection & ServicesSection

---
+++ src/assets/assets.js
@@ -0,0 +1,7 @@
+import logo from "./Studio-logo.png";
+import truck from "./truck.jpg";
+
+export const assets = {
+  logo,
+  truck,
+};
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,7 @@
-import React from 'react'
+import React from "react";
 
 const Home = () => {
-  return (
-    <div>
-      <h1>Home</h1>
-    </div>
-  )
-}
+  return <>Home</>;
+};
 
-export default Home
+export default Home;
